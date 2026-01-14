ff73132fcc512cd074d9f3eb5d29b1887f3f1d5d
roccostager <99860439+roccostager@users.noreply.github.com>
2026-01-14 17:29:08 +0100

    Create empty useGiphyURL hook skeleton

--- src/Giphy.jsx
+++ src/Giphy.jsx
@@ -1,4 +1,14 @@
+const useGiphyURL = () => {  // Custom Hook
+    const [giphyURL, setGiphyURL] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    return { giphyURL, error, loading };
+}
+
 function Giphy() {
+    const { giphyURL, error, loading } = useGiphyURL();
+
     return (<>
         <h1>Giphy!</h1>
     </>);
